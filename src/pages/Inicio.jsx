fbb9cb0853b293f8e928df0a235cb0c29e51bb40
Sconte <63359062+contesl@users.noreply.github.com>
2025-05-20 11:21:02 -0300

    PRIMER UPLOAD

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//cambio imagen
+const Inicio = () => {
+  return (
+    <main>
+      <figure className="contenedorImagen centered">
+        <img id="imagen-index" src="../img/PersonalStyle.jpg" alt="Personal Style" />
+      </figure>
+    </main>
+  );
+};
+
+export default Inicio;
